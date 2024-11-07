cdd7e63cf2a974d6530a5c3ba0eccf343b999cbb
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-11-07 13:21:11 +0100

    two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from "./navbar/navbar.component";
 import { HeaderComponent } from "./header/header.component";
+import {  FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,5 +16,27 @@ export class AppComponent {
   imageURL: string = 'https://www.html.it/app/uploads/2024/05/angular.png'
   isDisabled: boolean = true;
   isActive: boolean = true;
+  userName: string = 'John Doe'
+
+  clickButton() {
+    console.log('Button clicked')
+    this.isActive = false
+  };
+
+  keyEnter(event:any) {
+    if (event.keyCode == 13){
+      console.log('Enter key pressed')
+      this.isActive = true
+    }
+  };
+
+  keyFiltering(user:HTMLInputElement){
+    console.log(user.value)
+  }
+
+  changeUsername(username:HTMLInputElement){
+    this.userName = username.value
+    console.log(username.value)
+  }
 
 }
